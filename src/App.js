575de913fd719dbbd0f3f8cea3d4075685b98548
Denis <veremyov.of@gmail.com>
2021-08-16 00:40:41 +0300

    test 2.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {
   BrowserRouter as Router,
+  HashRouter,
   Switch,
   Route
 } from "react-router-dom";
@@ -15,7 +16,7 @@ function App() {
   const lists = useSelector(state => state.mainReducer.todoItems);
   return (
       <div className="container">
-        <Router>
+        <HashRouter>
           <Switch>
             <Route exact path="/">
               <Links />
@@ -26,7 +27,7 @@ function App() {
               </Route>
             )}
           </Switch>
-        </Router>
+        </HashRouter>
       </div>
   );
 }
