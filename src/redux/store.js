d48aad9fc77ae24aac51e88e224f4c203cc66459
Denis <veremyov.of@gmail.com>
2021-08-16 00:39:31 +0300

    test 2.0

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,8 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import mainReducer from './mainReducer';
-import todoPercent from './todoPercent';
 const rootReducer = combineReducers({
     mainReducer,
-    todoPercent
 })
 const store = createStore(rootReducer, applyMiddleware(logger));
 
